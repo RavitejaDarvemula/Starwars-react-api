960a991fe3dc7827160604b211fec060517ed2a0
Raviteja <ravitejadarvemula@gmail.com>
2022-12-12 14:25:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Item from "./MyItem";
+
+import React from "react";
+// class FilmItemRow extends React.Component {
+//   render() {
+//     return (
+//       <li>
+//         <a href={this.props.url}>{this.props.url}</a>
+//       </li>
+//     );
+//   }
+// }
+class Starwars extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loadedCharacter: false,
+      name: null,
+      height: null,
+      homeworld: null,
+      image: null,
+      species: null,
+      // films: [],
+    };
+  }
+  getNewCharacter() {
+    // console.log("Get the character from the button");
+    const randomize = Math.ceil(Math.random() * 88);
+    const url = `https://akabab.github.io/starwars-api/api/id/${randomize}.json`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          name: data.name,
+          height: data.height,
+          homeworld: data.homeworld,
+          // films: data.films,
+          image: data.image,
+          species: data.species,
+          loadedCharacter: true,
+        });
+      });
+  }
+  render() {
+    // const movies = this.state.films.map((film, i) => {
+    //   return <FilmItemRow key={i} url={film} />;
+    // });
+    return (
+      <div>
+        {this.state.loadedCharacter && (
+          <div>
+            <img src={this.state.image} alt="" />
+            <h1>Name:{this.state.name}</h1>
+            <p>Height:{this.state.height} </p>
+            <p>HomeWorld:{this.state.homeworld}</p>
+            <p>Species: {this.state.species}</p>
+            {/* <ul>{movies}</ul> */}
+          </div>
+        )}
+        <button
+          type="button"
+          className="btn"
+          onClick={() => this.getNewCharacter()}
+        >
+          Randomize Character
+        </button>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Starwars />
       </header>
     </div>
   );
